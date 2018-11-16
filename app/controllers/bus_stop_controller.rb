963c36d4babd06952e87f1dd723fe30914f618f6
Gisely <giselyalves13@hotmail.com>
2018-11-16 00:38:28 -0200

    views do bus_stop_controller criadas. Alguns erros identificados. Nada testado

--- app/controllers/bus_stop_controller.rb
+++ app/controllers/bus_stop_controller.rb
@@ -2,19 +2,35 @@ class BusStopController < ApplicationController
   class << self
     include Resources::HttpClient
     include ::SPTrans::HttpClientConfiguration
-    def search_for_lines
-        params[rua]
-        # params[px], params[py]
-        # TODO: usar metodo PARADAS/BUSCAR na sptrans enviando lat e long ou nome e numero da rua
-        # Retornar para view no mesmo controller
-        # Quando usuário clicar na linha, dar um get na action do buses_controller
+    #TODO: Criar rota para Resourses <ta dando erro não consegui resolver"
+    def index
+    end
+
+    def search_for_bus_stop
+      if sptrans_login
+        bus_stop_info_json = get("/Parada/Buscar?termosBusca=#{params[bus_stop_address]}") 
+        puts response
+        return @lines = search_for_lines(bus_stop_info_json[cp]) #cp = codigo da parada
+      else puts "erro com login na sptrans"
+      end
+      # TODO: usar metodo PARADAS/BUSCAR na sptrans enviando lat e long ou nome e numero da rua
+      # Retornar para view no mesmo controller
+      # Quando usuário clicar na linha, dar um get na action do buses_controller
+    end
+
+    def search_for_lines(bus_stop_code)
+      if sptrans_login
+        lines_info_json = get("/Previsao/Parada?codigoParada=#{bus_stop_code}}") 
+        return lines_info_json[l] # l = atributo de linhas no json
+      else puts "erro com login na sptrans"
+      end
     end
 
     def sptrans_login
-        #TODO: post/login/token
-        token ="9cf92b800603de7a266f4400062d865a67ee64eeb4b968b01d608ffa7806b092"
-        response = post("/Login/Autenticar?token=#{token}", payload: {}.to_json)
-        puts response
+      #TODO: post/login/token
+      token ="9cf92b800603de7a266f4400062d865a67ee64eeb4b968b01d608ffa7806b092"
+      response = post("/Login/Autenticar?token=#{token}", payload: {}.to_json)
+      puts response
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :buses
+  get 'bus_stop/search_for_bus_stop'
+  get 'bus_stop/index'
+  root 'bus_stop#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
