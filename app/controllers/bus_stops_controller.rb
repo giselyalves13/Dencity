c7f1c58eecd5881a13bc4901540c8354db47931e
Julia Litvinoff Justus <julialiff@hotmail.com>
2018-11-26 04:39:44 -0200

    Batendo na API PHP que bate na SPTrans

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'cgi'
+
 class BusStopsController < ApplicationController
     include Resources::HttpClient
     include ::Sptrans::HttpClientConfiguration
@@ -10,16 +13,19 @@ class BusStopsController < ApplicationController
       puts "METODO 1"
       termosBusca = params[:bus_stop_address]
       puts termosBusca
-      cookie = sptrans_login
-      if !cookie.nil?
+      # cookie = sptrans_login
+      # if !cookie.nil?
         # bus_stop_info_json = get(url: "v2.1/Parada/Buscar", params: {token: token})
-        puts cookie.to_json['set-cookie']
-        bus_stop_info_json = get(url: "v2.1/Parada/Buscar", params: { termosBusca: termosBusca, apiCredentials: cookie })
-        puts response
-        @lines = search_for_lines(bus_stop_info_json[cp]) #cp = codigo da parada
+        bus_stop_info_json = get(url: "", params: { termosBusca: termosBusca, api: "PARADAS" })
+        puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7'
+        puts bus_stop_info_json
+        puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7'
+        @lines = JSON.parse bus_stop_info_json.body
+        # @lines = @lines.to_s[8..-2].split("]").map { |ch| ch+"]"}
+        # @lines = search_for_lines(bus_stop_info_json[cp]) #cp = codigo da parada
         render bus_stops_search_for_bus_stop_path
-      else @lines[0] = "erro com login na sptrans"
-      end
+      # else @lines[0] = "erro com login na sptrans"
+      # end
       # TODO: usar metodo PARADAS/BUSCAR na sptrans enviando lat e long ou nome e numero da rua
       # Retornar para view no mesmo controller
       # Quando usuário clicar na linha, dar um get na action do buses_controller
@@ -33,21 +39,22 @@ class BusStopsController < ApplicationController
       end
     end
 
-    def sptrans_login
-      #TODO: post/login/token
-      puts "METODO 2"
-      token ="9cf92b800603de7a266f4400062d865a67ee64eeb4b968b01d608ffa7806b092"
-      payload = {}.to_json
-      puts '----------------------------------------------------------------------------------'
-      # response = post(url: "v2.1/Login/Autenticar?token={#{token}}")
-      response = post(url: "v2.1/Login/Autenticar", params: {token: token})
-      puts '********************************************'
-      puts JSON.parse response.body
-      puts response.headers.to_json
-      puts response.status.to_s
-      puts '********************************************'
-      JSON.parse response.headers.to_json
-    end
+    # def sptrans_login
+    #   #TODO: post/login/token
+    #   puts "METODO 2"
+    #   token ="9cf92b800603de7a266f4400062d865a67ee64eeb4b968b01d608ffa7806b092"
+
+    #   payload = {}.to_json
+    #   puts '----------------------------------------------------------------------------------'
+    #   # response = post(url: "v2.1/Login/Autenticar?token={#{token}}")
+    #   response = post(url: "v0/Login/Autenticar", params: {token: token})
+    #   puts '********************************************'
+    #   puts JSON.parse response.body
+    #   puts response.headers.to_json
+    #   puts response.status.to_s
+    #   puts '********************************************'
+    #   JSON.parse response.headers.to_json
+    # end
 
     # helper_method :search_for_bus_stop
 end
--- config/initializers/sptrans.rb
+++ config/initializers/sptrans.rb
@@ -1,4 +1,4 @@
 Sptrans.configure do |config|
-  config.base_url = "http://api.olhovivo.sptrans.com.br/"
+  config.base_url = "http://localhost:8000/"
   config.token = "9cf92b800603de7a266f4400062d865a67ee64eeb4b968b01d608ffa7806b092"
 end
